ffd9fcb97f7e9333717816f08c5afd7712d0ff32
renato nascimento <renatonascto@gmail.com>
2018-08-21 21:43:28 -0400

    adding composer.lock to .gitignore

--- test/gilded_rose_test.php
+++ test/gilded_rose_test.php
@@ -10,7 +10,7 @@ class GildedRoseTest extends TestCase {
     function testRegularItemBeforeSellInDate() {
         $items = array(new Item("foo", 5, 2));
         $gildedRose = new GildedRose($items);
-        $gildedRose->update_quality();
+        $gildedRose->updateQuality();
         $this->assertEquals(4, $items[0]->sell_in);
         $this->assertEquals(1, $items[0]->quality);
     }
@@ -18,7 +18,7 @@ class GildedRoseTest extends TestCase {
     function testRegularItemAfterSellInDate() {
         $items = array(new Item("foo", -1, 2));
         $gildedRose = new GildedRose($items);
-        $gildedRose->update_quality();
+        $gildedRose->updateQuality();
         $this->assertEquals(-2, $items[0]->sell_in);
         $this->assertEquals(0, $items[0]->quality);
     }
@@ -26,7 +26,7 @@ class GildedRoseTest extends TestCase {
     function testItemQualityNeverNegative() {
         $items = array(new Item("foo", -1, 1));
         $gildedRose = new GildedRose($items);
-        $gildedRose->update_quality();
+        $gildedRose->updateQuality();
         $this->assertEquals(-2, $items[0]->sell_in);
         $this->assertEquals(0, $items[0]->quality);
     }    
@@ -34,7 +34,7 @@ class GildedRoseTest extends TestCase {
     function testAgedItemsBeforeSellIn() {
         $items = array(new Item("Aged Brie", 1, 1));
         $gildedRose = new GildedRose($items);
-        $gildedRose->update_quality();
+        $gildedRose->updateQuality();
         $this->assertEquals(0, $items[0]->sell_in);
         $this->assertEquals(2, $items[0]->quality);
     }
@@ -42,7 +42,7 @@ class GildedRoseTest extends TestCase {
     function testAgedItemsAfterSellIn() {
         $items = array(new Item("Aged Brie", -1, 3));
         $gildedRose = new GildedRose($items);
-        $gildedRose->update_quality();
+        $gildedRose->updateQuality();
         $this->assertEquals(-2, $items[0]->sell_in);
         $this->assertEquals(5, $items[0]->quality);
     }
@@ -50,7 +50,7 @@ class GildedRoseTest extends TestCase {
     function testAgedItemsMaxQuality() {
         $items = array(new Item("Aged Brie", -1, 50));
         $gildedRose = new GildedRose($items);
-        $gildedRose->update_quality();
+        $gildedRose->updateQuality();
         $this->assertEquals(-2, $items[0]->sell_in);
         $this->assertEquals(50, $items[0]->quality);
     }
@@ -58,7 +58,7 @@ class GildedRoseTest extends TestCase {
     function testLegendaryItemsQuality() {
         $items = array(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
         $gildedRose = new GildedRose($items);
-        $gildedRose->update_quality();
+        $gildedRose->updateQuality();
         $this->assertEquals(0, $items[0]->sell_in);
         $this->assertEquals(80, $items[0]->quality);
     }
@@ -66,7 +66,7 @@ class GildedRoseTest extends TestCase {
     function testTicketsMoreThanTenDaysBeforeSellIn() {
         $items = array(new Item("Backstage passes to a TAFKAL80ETC concert", 11, 11));
         $gildedRose = new GildedRose($items);
-        $gildedRose->update_quality();
+        $gildedRose->updateQuality();
         $this->assertEquals(10, $items[0]->sell_in);
         $this->assertEquals(12, $items[0]->quality);
     }
@@ -74,7 +74,7 @@ class GildedRoseTest extends TestCase {
     function testTicketsBetweenTenDaysAndFiveDaysBeforeSellIn() {
         $items = array(new Item("Backstage passes to a TAFKAL80ETC concert", 9, 11));
         $gildedRose = new GildedRose($items);
-        $gildedRose->update_quality();
+        $gildedRose->updateQuality();
         $this->assertEquals(8, $items[0]->sell_in);
         $this->assertEquals(13, $items[0]->quality);
     }
@@ -82,7 +82,7 @@ class GildedRoseTest extends TestCase {
     function testTicketsFiveDaysBeforeSellIn() {
         $items = array(new Item("Backstage passes to a TAFKAL80ETC concert", 4, 11));
         $gildedRose = new GildedRose($items);
-        $gildedRose->update_quality();
+        $gildedRose->updateQuality();
         $this->assertEquals(3, $items[0]->sell_in);
         $this->assertEquals(14, $items[0]->quality);
     }
@@ -90,7 +90,7 @@ class GildedRoseTest extends TestCase {
     function testTicketsAfterSellIn() {
         $items = array(new Item("Backstage passes to a TAFKAL80ETC concert", 0, 11));
         $gildedRose = new GildedRose($items);
-        $gildedRose->update_quality();
+        $gildedRose->updateQuality();
         $this->assertEquals(-1, $items[0]->sell_in);
         $this->assertEquals(0, $items[0]->quality);
     }    
@@ -98,7 +98,7 @@ class GildedRoseTest extends TestCase {
 //    function testConjuredItemsBeforeSellIn() {
 //        $items = array(new Item("Conjured Mana Cake", 3, 11));
 //        $gildedRose = new GildedRose($items);
-//        $gildedRose->update_quality();
+//        $gildedRose->updateQuality();
 //        $this->assertEquals(2, $items[0]->sell_in);
 //        $this->assertEquals(9, $items[0]->quality);
 //    }
